3e81e8967e98db457967f3161df1ceac46b27102
Trugamr <trugamr@live.in>
2021-07-30 00:03:19 +0530

    tailwind setup

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
