4168f790a5937545c3ad5c0e09e355b6f8c2b358
Trugamr <trugamr@live.in>
2021-07-30 00:50:59 +0530

    eslint setup

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      <h1>Hello</h1>
-    </div>
+    <div className="h-full w-full flex items-center justify-center bg-black" />
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    eslintPlugin({
+      cache: false,
+    }),
+  ],
 })
